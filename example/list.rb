8b6afab845b7169bd8899b74670033f8204d6134
Dominik Richter <dominik.richter@gmail.com>
2013-12-18 13:09:48 +0100

    print the selected index of the list in the example
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- example/list.rb
+++ example/list.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/inquirer'
-List.ask "Look behind you...", [
+idx = List.ask "Look behind you...", [
   "a three-headed monkey!",
   "a pink pony",
   "Godzilla"
 ]
+puts "you selected #{idx}"
