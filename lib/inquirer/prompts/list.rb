7e436ff5dc28510123e24c31914742114795e97e
Dominik Richter <dominik.richter@gmail.com>
2013-12-18 04:22:11 +0100

    improvement: separate list rendering from processing
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- lib/inquirer/prompts/list.rb
+++ lib/inquirer/prompts/list.rb
@@ -1,25 +1,50 @@
+module ListRendererBase
+
+  def render heading = nil, list = [], footer = nil
+    ( heading.nil? ? "" : self::Heading % heading ) +
+    list.map do |li|
+      li["selected"] ?
+        self::SelectedItem % li["value"] :
+        self::Item % li["value"]
+    end.join("") +
+    ( footer.nil? ? "" : self::Footer % footer )
+  end
+end
+
+module ListRendererSimple
+  include ListRendererBase
+  extend self
+  Heading = "%s:\n"
+  Footer = "%s\n"
+  Item = "  %s\n"
+  SelectedItem = "x %s\n"
+end
+
 class List
-  
-  def initialize elements
+  def initialize elements, question = nil, renderer = ListRendererSimple
     @elements = elements
+    @question = question
     @pos = 0
     @prompt = ""
+    @renderer = renderer
   end
 
   def render
-    items = @elements.map{|e| "  "+e}
-    items[@pos][0] = ">"
-
-    @prompt = (
-      ["Please choose:"] + 
-      items  
-      ).join("\n")
-
+    # transform the list into
+    # {"value"=>..., "selected"=> true|false}
+    e = @elements.
+      # attach the array position
+      map.with_index(0).
+      map do |c,pos|
+        { "value"=>c, "selected" => pos == @pos }
+      end
+    # call the renderer
+    @prompt = @renderer.render(@question, e)
     puts @prompt
   end
 
   def clear
-    puts "\r" + ( "\e[A" * ( @prompt.scan(/\n/).length + 2 ))
+    puts "\r" + ( "\e[A" * ( @prompt.scan(/\n/).length + 1 ))
   end
 
   def run
