74cb1309e9d48e7ddfa526560c28da9e4c82e80f
Dominik Richter <dominik.richter@gmail.com>
2013-12-21 02:16:58 +0100

    improvement: extract style into its own module
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

---
+++ lib/inquirer/style.rb
@@ -0,0 +1,16 @@
+module Inquirer::Style
+  Style = Struct.new(:selector, :checkbox_on, :checkbox_off)
+  extend self
+
+  Simple = Style.new(
+    ">",
+    "[x]",
+    "[ ]"
+  )
+
+  Default = Style.new(
+    "‣",
+    "⬢",
+    "⬡"
+  )
+end
