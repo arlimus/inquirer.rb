b6f9f9e7644a84394590ba5d948d7976e5455854
Juan Ignacio Donoso <jidonoso@gmail.com>
2014-03-22 14:19:01 -0300

    Fix the clear method not clearing every line on the way up

--- lib/inquirer/utils/iohelper.rb
+++ lib/inquirer/utils/iohelper.rb
@@ -100,7 +100,7 @@ module IOHelper
     # determine how many lines to move up
     n = @rendered.scan(/\n/).length
     # jump back to the first position and clear the line
-    print carriage_return + ( line_up * n ) + clear_line
+    print carriage_return + ( line_up + clear_line ) * n
   end
 
   # hides the cursor and ensure the curso be visible at the end
